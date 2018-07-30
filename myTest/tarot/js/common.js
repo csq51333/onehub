4868f97553fad79739924f7ae150f8779d0fdfae
csq <csq51333@163.com>
2018-07-30 13:59:56 +0800

    来自分支的最新代码

--- myTest/tarot/js/common.js
+++ myTest/tarot/js/common.js
@@ -13,6 +13,10 @@ function getParmas(){
 }
 
 function angleZ(el){
+	var checkStr = el.style.transform;
+	if(!checkStr || !/rotate/.test(checkStr)){
+		return ''
+	}
 	var str = el.style.transform.replace(/([a-zA-Z]+|\(+|\)+)/g,'')
 	if(/\s+/.test(str)){
 		var arr = str.split(' ')
@@ -25,4 +29,20 @@ function getRoutate(str,ag){
 	var re = new RegExp("(?<=rotate" + ag + "?\\()(-?\\d+\\.?\\d*)(?=deg\\))")
 	var angle = new Number(str.match(re)[0])
 	return angle;
+}
+
+function SZNWei(_ag){
+	if(typeof(_ag) == "number"){
+		_ag %= 360
+		ag = _ag < 0 ? _ag + 360 : _ag;
+		return (ag >= 90 && ag < 270 || ag <= -90 && ag > -270) ? 180 : 0;
+	}
+}
+
+function HZNWei(_ag){
+	if(typeof(_ag) == "number"){
+		_ag %= 360
+		ag = _ag < 0 ? _ag + 360 : _ag;
+		return ag >= 0 && ag < 180 ? 90 : 270;
+	}
 }
